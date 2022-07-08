96258d32c081257dfce67408d6b6e719bdde527e
jeoh06 <84434560+jeoh06@users.noreply.github.com>
2022-07-08 14:46:42 +0900

    Add files via upload

---
+++ brochure/brochure.js
@@ -0,0 +1,4 @@
+/* Features to add
+Hide navbar while scrolling down
+Content appears dynamically while scrolling
+Scroll loop on art gallery
---
+++ brochure/navbar.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navbar</title>
+</head>
+<body>
+<nav id="navbar">
+        <a href="#aboutSPPS"><img id="logo" src="images/SP logo.png" alt="SPPS logo"></a>
+        <ul class="menu-list">
+            <li><a href="#aboutSPPS">About SPPS</a></li>
+            <li><a href="#academics">Academics</a></li>
+            <li><a href="#studentLife">Student Life</a></li>
+            <li><a href="#counseling">Counseling</a></li>
+            <li><a href="#art">Art</a></li>
+        </ul>
+    </nav>
+    <nav id="nav-mobile">
+        <div class="navbar">
+            <div class="nav-container">
+                <input class="checkbox" type="checkbox" name="" id="">
+                <div class="hamburger-lines">
+                    <span class="line line1"></span>
+                    <span class="line line2"></span>
+                    <span class="line line3"></span>
+                </div>
+                <ul class="menu-items">
+                    <li><a href="#aboutSPPS">About SPPS</a></li>
+                    <li><a href="#academics">Academics</a></li>
+                    <li><a href="#studentLife">Student Life</a></li>
+                    <li><a href="#counseling">Counseling</a></li>
+                    <li><a href="#art">Art</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</body>
+</html>
