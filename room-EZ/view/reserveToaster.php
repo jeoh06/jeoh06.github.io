2dda31dfa6916f056c81e6203f8e7490de9a2740
Jeong Geon Oh <84434560+jeoh06@users.noreply.github.com>
2022-09-07 12:49:27 +0900

    Delete room-EZ directory

--- room-EZ/view/reserveToaster.php
+++
@@ -1,5 +0,0 @@
-<div class="reserveToast">
-
-<?= ($_REQUEST['action'] == "reserveComplete") ? "Reservation Complete! Now listed on your profile page." : "Reservation incomplete."; ?>
-
-</div>
--- room-EZ/view/toaster.php
+++
@@ -1,5 +0,0 @@
-<div class="toast">
-
-<?= ($_REQUEST['action'] == "loggedIn") ? "You Are Logged In !" : "You Are Logged Out !"; ?>
-
-</div>
